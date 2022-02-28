e91348367bc42b6162fc8e59a0b7cfc70c6007cb
shishkin_i <qwertyshishkin@yandex.ru>
2022-02-28 22:50:32 +0300

    все обернуто в красивые функции и работает притом хорошо

--- OpenGL_Project/Textures.h
+++ OpenGL_Project/Textures.h
@@ -7,6 +7,8 @@
 #include <GLFW/glfw3.h>
 #include"Shader.h"
 
+#include <cstdarg>
+
 //все о текстурах: обычно цвета нормализуются, в gl_rgba32f не нормализуются
 // параметры:https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glTexImage2D.xhtml
 //  texelFetch позволяет передавать в аргументах значения как для массива
@@ -26,17 +28,23 @@ class Textures
 	ShProg &ourShader;
 	Sphere sph0;
 	Box box0;
+	unsigned int id = 0;
 	unsigned int ID_texDataSph{ 0 };
 	unsigned int ID_texColor{ 0 };
 	unsigned int texture_array{0};
+
+	unsigned int idArr[32]{ 0 };
+	unsigned int locArr[32]{ 0 };
+	GLenum targetArr[32];
+
 	long spheresSize = 0;
 	long boxesSize = 0;
 	const int sizey = 1;
-	std::vector<float> out;
+	vector<float> out;
+	vector<float> colorOut;
 	int ID_sizeSph = glGetUniformLocation(ourShader.getID(), "sizeSph");
 	int ID_sizeSphes = glGetUniformLocation(ourShader.getID(), "SphBeg_to_End");
 	int ID_sizeBoxes = glGetUniformLocation(ourShader.getID(), "BoxBeg_to_End");
-	vector<float> colorOut;
 
 
 public:
@@ -86,17 +94,72 @@ public:
 		}
 		boxesSize = out.size();
 	}
+	void createTex(GLenum t1, unsigned int type, int width, int height=0, int depth=0)
+	{
+		glActiveTexture(GL_TEXTURE0 + id);
+		glGenTextures(1, &(idArr[id]));
+		glBindTexture(t1, idArr[id]);
+		glTexParameteri(t1, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//см ниже
+		glTexParameteri(t1, GL_TEXTURE_MAG_FILTER, GL_NEAREST);//пиксель на всей области одного цвета
+		if (t1 == GL_TEXTURE_1D || t1 == GL_TEXTURE_BUFFER)
+			glTexImage1D(t1, 0, type, width, 0, GL_RGBA, GL_FLOAT, idArr);//заполняется мусором
+		else if (t1 == GL_TEXTURE_2D || t1 == GL_TEXTURE_1D_ARRAY || t1 == GL_TEXTURE_RECTANGLE)
+		{
+			glTexImage2D(t1, 0, type, width, height, 0, GL_RGBA, GL_FLOAT, 0);//заполняется мусором
+
+		}
+		else if (t1 == GL_TEXTURE_3D || t1 == GL_TEXTURE_2D_ARRAY)
+			glTexImage3D(t1, 0, type, width,height,depth, 0, GL_RGBA, GL_FLOAT, idArr);//заполняется мусором
+		glUniform1i(locArr[id], id);
+		targetArr[id] = t1;
+		id++;
+	}
+	void setloc(string str)
+	{
+		locArr[id] = glGetUniformLocation(ourShader.getID(), str.c_str());
+	}
+	void setloc(int id2)
+	{
+		locArr[id] = id2;
+	}
+	//условие ввода: сначала размеры затем отступ
+	//в зависимости от размерности выбирается какие переменные куда поставить
+	//пример:
+	//insDataTex<2, float>(targetid, data, sizex, sizey, offsetx, offsety);
+	// можно было бы сделать элипсис, но там не понятно кончились ли числа или мы их не ввели так как они 0
+	// 
+	template<class cl1,int dim = 1,GLenum type = GL_FLOAT>
+	void insDataTex(int id2, cl1* dataIn, int in1 = 0, int in2 = 0, int in3 = 0, int in4 = 0, int in5 = 0, int in6 = 0 )
+	{
+		glActiveTexture(GL_TEXTURE0 + id2);
+		glBindTexture(targetArr[id2], idArr[id2]);
+		if (dim == 1)
+			glTexSubImage1D(targetArr[id2], 0, in2,in1, GL_RGBA, type, dataIn);
+		else if (dim == 2)
+			glTexSubImage2D(targetArr[id2], 0, in3, in4, in1, in2, GL_RGBA, type, dataIn);
+		else if (dim == 3)
+			glTexSubImage3D(targetArr[id2], 0, in4,in5,in6,in1,in2,in3, GL_RGBA, type, dataIn);
+	}
+	void sendTex(int id)
+	{
+
+		glActiveTexture(GL_TEXTURE0+id);
+		glBindTexture(targetArr[id], idArr[id]);
+	}
 	void start()
 	{	
 		//временно:
 		colorOut.resize(1);
 
-		glActiveTexture(GL_TEXTURE0);
-		glGenTextures(1, &ID_texDataSph);
-		glBindTexture(GL_TEXTURE_2D, ID_texDataSph);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//см ниже
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);//пиксель на всей области одного цвета
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, out.size(), sizey, 0, GL_RGBA, GL_FLOAT, &out[0]);
+		setloc("Content");
+		createTex(GL_TEXTURE_2D, GL_RGBA32F, out.size(), sizey);
+		insDataTex<float,2>(0, &out[0],out.size(), sizey);
+		//glActiveTexture(GL_TEXTURE0+id);
+		//glGenTextures(1, &(idArr[id]));
+		//glBindTexture(GL_TEXTURE_2D, idArr[id]);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//см ниже
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);//пиксель на всей области одного цвета
+		//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, out.size(), sizey, 0, GL_RGBA, GL_FLOAT, &out[0]);
 
 		glActiveTexture(GL_TEXTURE1);
 		glGenTextures(1, &ID_texColor);
@@ -110,7 +173,7 @@ public:
 		}
 		float arr2[8]{ 0.1,0.2,0.3,0.4,0.0,0.0,0.0,0.8 };
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, 1, 2, 0, GL_RGBA, GL_FLOAT, arr2);
-		glUniform1i(glGetUniformLocation(ourShader.getID(), "Content"), 0);
+		//glUniform1i(glGetUniformLocation(ourShader.getID(), "Content"), 0);
 		glUniform1i(glGetUniformLocation(ourShader.getID(), "ColorsTex"), 1);
 
 
@@ -134,48 +197,11 @@ public:
 		};
 
 		glUniform1i(10, 3);
-		//glActiveTexture(GL_TEXTURE1);
-		//glActiveTexture(GL_TEXTURE3);
-		//float ans;
-		//glGetFloatv(GL_ACTIVE_TEXTURE, &ans);
-		//cout << "texnow" << ans << "\n";
 		glGenTextures(1, &texture);
-		glBindTexture(GL_TEXTURE_3D, texture);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//см ниже
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);//пиксель на всей области одного цвета
-		glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA8, 2, 2, 2, 0, GL_RGBA, GL_FLOAT, texels);
-
-		//GLsizei width = 2;
-		//GLsizei height = 2;
-		//GLsizei layerCount = 2;
-		//GLsizei mipLevelCount = 1;
-
-		//// Read you texels here. In the current example, we have 2*2*2 = 8 texels, with each texel being 4 GLubytes.
-		//float texelso[32]{ 0 };
-
-		////glActiveTexture(GL_TEXTURE2);
-		//glGenTextures(1, &texture);
-		//glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
-		//// Allocate the storage.
-		//glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGBA, 1, 1, 1, 0, GL_RGBA, GL_FLOAT, texels);
-		//// Upload pixel data.
-		//// The first 0 refers to the mipmap level (level 0, since there's only 1)
-		//// The following 2 zeroes refers to the x and y offsets in case you only want to specify a subrectangle.
-		//// The final 0 refers to the layer index offset (we start from index 0 and have 2 levels).
-		//// Altogether you can specify a 3D box subset of the overall texture, but only one mip level at a time.
-		////glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, 0, 2, 2, 2, GL_RGBA, GL_FLOAT, texels);
-		//
-
-
-		//// Always set reasonable texture parameters
-		//glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		//glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		//glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		//glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		//cout << glGetUniformLocation(ourShader.getID(), "texture_array") << "\n";
-		//glUniform1i(glGetUniformLocation(ourShader.getID(), "texture_array"), GL_TEXTURE_2D_ARRAY-GL_TEXTURE0);
-		////glActiveTexture(GL_TEXTURE2);
-		//glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
+		glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
+		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//см ниже
+		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);//пиксель на всей области одного цвета
+		glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGBA8, 2, 2, 2, 0, GL_RGBA, GL_FLOAT, texels);
 	}
 	template<class  cl1,int len>
 	void testData(GLenum target)
@@ -203,13 +229,16 @@ public:
 
 
 
-		testData<float, 32>(GL_TEXTURE_3D);
+		testData<float, 32>(GL_TEXTURE_2D_ARRAY);
+
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, ID_texColor);
+		sendTex(0);
+		/*
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, ID_texDataSph);
+		glBindTexture(GL_TEXTURE_2D, ID_texDataSph);*/
 		glActiveTexture(GL_TEXTURE3);
-		glBindTexture(GL_TEXTURE_3D,texture );
+		glBindTexture(GL_TEXTURE_2D_ARRAY,texture );
 
 		glUniform2i(ID_sizeSph, sph0.size, sizey);
 		glUniform2i(ID_sizeSphes, 0, spheresSize);
